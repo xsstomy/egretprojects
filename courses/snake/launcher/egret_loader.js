63e93662a1aae466f8d9c57373fea85bf2e23072
xiashi <674371968@qq.com>
2016-12-28 19:21:52 +0800

    [u] add snake

---
+++ courses/snake/launcher/egret_loader.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2014,Egret-Labs.org
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Egret-Labs.org nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY EGRET-LABS.ORG AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL EGRET-LABS.ORG AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+egret_h5.startGame = function () {
+    var  context = egret.MainContext.instance;
+    context.touchContext = new egret.HTML5TouchContext();
+    context.deviceContext = new egret.HTML5DeviceContext();
+    context.netContext = new egret.HTML5NetContext();
+
+    /*egret.StageDelegate.getInstance().setDesignSize(1024, 768);
+    context.stage = new egret.Stage();
+    var scaleMode =  egret.MainContext.deviceType == egret.MainContext.DEVICE_MOBILE ? egret.StageScaleMode.SHOW_ALL : egret.StageScaleMode.NO_SCALE;
+    context.stage.scaleMode = scaleMode;*/
+
+    var width_s = 1920;
+    var height_s = 1080;
+    egret.StageDelegate.getInstance().setDesignSize(width_s, height_s);
+    context.stage = new egret.Stage();
+    var scaleMode =  egret.MainContext.deviceType == egret.MainContext.DEVICE_MOBILE ? egret.StageScaleMode.NO_BORDER : egret.StageScaleMode.NO_SCALE;
+    context.stage.scaleMode = scaleMode;
+
+    var container = new egret.EqualToFrame();
+
+    var scaleX=document.documentElement.clientWidth/width_s;
+    var scaleY=document.documentElement.clientHeight/height_s;
+    //alert(document.documentElement.clientWidth +"*"+document.documentElement.clientHeight+" "+scaleX+"*"+scaleY);
+
+    var content = new egret.FixedSize(width_s*Math.min(scaleX, scaleY), height_s*Math.min(scaleX, scaleY));
+    //if(scaleX<scaleY){
+    //    content = new egret.FixedWidth();
+    //}
+    //else{
+    //    content = new egret.FixedHeight();
+    //}
+    var policy = new egret.ResolutionPolicy(container,content)
+    egret.StageDelegate.getInstance()._setResolutionPolicy(policy)
+    //WebGL是egret的Beta特性，默认关闭
+    var rendererType = 0;
+    if (rendererType == 1) {// egret.WebGLUtils.checkCanUseWebGL()) {
+        context.rendererContext = new egret.WebGLRenderer();
+    }
+    else {
+        context.rendererContext = new egret.HTML5CanvasRenderer();
+    }
+
+    egret.MainContext.instance.rendererContext.texture_scale_factor = 1;
+    context.run();
+
+    var rootClass;
+    if(document_class){
+        rootClass = egret.getDefinitionByName(document_class);
+    }
+    if(rootClass) {
+        var rootContainer = new rootClass();
+        if(rootContainer instanceof egret.DisplayObjectContainer){
+            context.stage.addChild(rootContainer);
+        }
+        else{
+            throw new Error("文档类必须是egret.DisplayObjectContainer的子类!");
+        }
+    }
+    else{
+        throw new Error("找不到文档类！");
+    }
+};
